4aec10a5733c7cfe114112c800a834c916775fd7
Вадим Киселев <vadimkiselev@Vadims-MacBook-Pro.local>
2018-07-11 23:19:40 +0400

    Initiate animation text

--- js/code.js
+++ js/code.js
@@ -1,25 +1,60 @@
 function entry() {
-    $('#1').text('Browser name: ' + visitor.browser.name + ' ' + visitor.browser.versionMajor + '.' + visitor.browser.versionMinor);
+    let browser = 'Browser name: ' + visitor.browser.name + ' ' + visitor.browser.versionMajor + '.' + visitor.browser.versionMinor;
 
-    $('#2').text('Device: ' + visitor.device.name + ' : ' + visitor.device.screen.resolution + ' - ' + visitor.device.screen.width + 'x' + visitor.device.screen.height);
+    let device = 'Device: ' + visitor.device.name + ' : ' + visitor.device.screen.resolution + ' - ' + visitor.device.screen.width + 'x' + visitor.device.screen.height;
 
-    $('#3').text('First session: ' + visitor.firstSession.sessionStart + ' - '
-        + visitor.firstSession.referral.url.path); // ?
-
-    $('#4').text('Geo: ' + visitor.geo.continentName + ' (' + visitor.geo.continentCode + '), '
+    let geo = 'Geo: ' + visitor.geo.continentName + ' (' + visitor.geo.continentCode + '), '
         + visitor.geo.countryName + ' (' + visitor.geo.countryCode + '), '
-        + visitor.geo.city + ' [' + visitor.geo.coordinates.latitude + ' - ' + visitor.geo.coordinates.longitude + ']');
+        + visitor.geo.city + ' [' + visitor.geo.coordinates.latitude + ' - ' + visitor.geo.coordinates.longitude + ']';
 
-    $('#5').text('IP address: ' + visitor.ip.address);
+    let ip = 'IP address: ' + visitor.ip.address;
 
-    $('#6').text('Locale: ' + visitor.locale.countryCode + ' ' + visitor.locale.languageCode);
+    let locale = 'Locale: ' + visitor.locale.countryCode + ' ' + visitor.locale.languageCode;
 
-    $('#7').text('OS: ' + visitor.os.name + ' ' + visitor.os.version);
+    let os = 'OS: ' + visitor.os.name + ' ' + visitor.os.version;
+}
 
-    $('#8').text('Session: ' + visitor.session.sessionStart + ' - '
-        + visitor.session.referral.url.path); // ?
+function animate(element, text, opacity_from, opacity_to, callback) {
+    let $element = $(element);
+    $element.text(text);
+
+    let timeout = 1500;
+
+    $element.animate({
+        opacity: opacity_from,
+    }, timeout, function () {
+        $element.animate({
+            opacity: opacity_to,
+        }, timeout, callback);
+    });
+}
+
+function schedule(element, lines) {
+    let size = lines.length;
+
+    let chain = new Array(size);
+    chain[size - 1] = function () {
+        animate(element, lines[size - 1], 1, 0);
+    };
+
+    for (let index = size - 2; index >= 0; index--) {
+        chain[index] = function () {
+            animate(element, lines[index], 1, 0, chain[index + 1]);
+        }
+    }
+
+    chain[0](); // start chain
 }
 
 $(window).on("load", function() {
     entry();
+
+    schedule($('#text'), [
+        'Свободный интернет',
+        'Свободный интернет',
+        'Свободный интернет',
+        'Свободный интернет',
+        'Свободный интернет',
+        'Свободный интернет'
+    ]);
 });
